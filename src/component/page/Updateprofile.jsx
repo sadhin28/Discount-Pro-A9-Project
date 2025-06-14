1b2803efd342475888dfbf5a9c7f3602815ddd2d
sadhin28 <taosifsadhin113@gmail.com>
2025-06-14 23:05:31 +0600

    update profile

--- src/component/page/Updateprofile.jsx
+++ src/component/page/Updateprofile.jsx
@@ -1,9 +1,64 @@
-import React from 'react';
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../../provider/AuthProvider";
+import toast from "daisyui/components/toast";
 
 const UpdateProfile = () => {
+    const navigate = useNavigate();
+    const { updateUserProfile } =useContext(AuthContext)
+      const handleUpdate = (event) => {
+        event.preventDefault();
+        const name = event.target.name.value;
+        const photo = event.target.photo.value;
+        updateUserProfile(name, photo)
+            .then(() => {
+                console.log("Profile updated successfully");
+                navigate("/profile");
+                toast.success("Profile updated successfully")
+            })
+
+
+    };
     return (
-        <div>
-            <h1>updateProfile</h1>
+         <div className="min-h-screen bg-gray-100 flex justify-center items-center">
+           
+            <div className="bg-white p-10 rounded-xl shadow-lg max-w-md w-full">
+                <h2 className="text-center text-2xl font-bold mb-4">Update Profile</h2>
+                <form onSubmit={handleUpdate} className="space-y-4">
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Photo URL</span>
+                        </label>
+                        <input
+                            type="url"
+                            placeholder="Enter photo URL"
+                            className="input input-bordered w-full"
+                            name="photo"
+                            required
+                        />
+                    </div>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Name</span>
+                        </label>
+                        <input
+                            type="text"
+                            placeholder="Enter name"
+                            className="input input-bordered w-full"
+                            name="name"
+                            required
+                        />
+                    </div>
+                    <div className="mt-4">
+                        <button
+                            type="submit"
+                            className="btn bg-[#19BC9B] mt-5 text-white w-full"
+                        >
+                            Update Information
+                        </button>
+                    </div>
+                </form>
+            </div>
         </div>
     );
 };
