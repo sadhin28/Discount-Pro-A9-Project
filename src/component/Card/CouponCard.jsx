7d1492a8f8814dd608cdb283d9b8b99d88a807ec
sadhin28 <taosifsadhin113@gmail.com>
2025-06-14 22:51:29 +0600

    add coupon section

--- src/component/Card/CouponCard.jsx
+++ src/component/Card/CouponCard.jsx
@@ -1,9 +1,33 @@
 import React from 'react';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
+import { toast } from 'react-toastify';
 
-const CouponCard = () => {
+const CouponCard = ({coupon }) => {
+      const notify = () => toast.success("Coupon code copied! 🎉");
+
     return (
-        <div>
-            <h1>CouponCard</h1>
+        <div className="border bg-[#E6F0F3] mt-4 border-gray-300 flex flex-col rounded-lg p-4 shadow-md hover:shadow-lg transition-shadow">
+
+            <div className="flex-1">
+                <h3 className="text-lg font-semibold text-gray-800 mb-2">{coupon.description}</h3>
+                <p className="text-sm text-gray-600 mb-1">
+                    <span className="font-medium">Expiry Date:</span> {coupon.expiry_date}
+                </p>
+                <p className="text-sm text-gray-600 mb-1">
+                    <span className="font-medium">Condition:</span> {coupon.condition}
+                </p>
+                <p className="text-sm text-gray-600 mb-3">
+                    <span className="font-medium">Coupon Type:</span> {coupon.coupon_type}
+                </p>
+            </div>
+            <div className="px-8 rounded-md">
+                <div className="flex gap-2 justify-between items-center border-2 border-dashed bg-white px-1">
+                    <h3 className="text-lg font-semibold text-gray-500">{coupon.coupon_code}</h3>
+                    <CopyToClipboard text={coupon.coupon_code}>
+                        <button className="btn btn-sm bg-[#19BC9B] rounded-md text-white" onClick={notify}>Copy</button>
+                    </CopyToClipboard>
+                </div>
+            </div>
         </div>
     );
 };
--- src/component/page/CouponPage.jsx
+++ src/component/page/CouponPage.jsx
@@ -1,9 +1,61 @@
 import React from 'react';
-
+import { AiOutlineDislike, AiOutlineLike } from 'react-icons/ai';
+import { useLoaderData, useLocation, useNavigate } from 'react-router-dom';
+import CouponCard from '../Card/CouponCard';
+import { RxCross2 } from 'react-icons/rx';
+import ReactStars from "react-rating-stars-component";
 const CouponPage = () => {
+    const brand = useLoaderData();
+    const {brand_name, rating, description, brand_logo, coupons, shop_link }=brand
+    const firstExample = {
+        size: 20,
+        value: rating,
+        edit: false
+    };
+    const navigate = useNavigate()
+    const location =useLocation()
+    const handleClose =()=>{
+        if(location.state){
+            navigate('/')
+        }else{
+            navigate(-1)
+        }
+    }
     return (
-        <div>
-            <h1>CouponPage</h1>
+       <div className="p-7 bg-base-200 w-11/12  lg:w-1/2 mx-auto my-10 flex flex-col  justify-center items-center rounded-lg shadow-2xl relative">
+           
+            <div className="absolute btn btn-circle btn-sm btn-error -top-2 -right-2" onClick={handleClose}>
+                <RxCross2 />
+            </div>
+            <div className="flex items-center gap-2 justify-center flex-col">
+                <img src={brand_logo} alt={brand_name} className="w-40" />
+                <h3 className="text-2xl font-bold text-center">{brand_name}</h3>
+                <p className="text-gray-700 text-xl text-center">{description}</p>
+                <ReactStars {...firstExample} />
+                <small className="font-semibold text-[#0501ff]">Rating:{rating}</small>
+
+            </div>
+            <div className="grid md:grid-cols-2 gap-4">
+                {
+                    coupons?.map((coupon, index) => (
+                        <CouponCard key={index} coupon={coupon} />
+                    ))
+                }
+            </div>
+            <a href={shop_link} target="_blank" className="btn px-20 mt-4 btn-outline hover:bg-[#19BC9B] hover:border-none shadow-md border-[#19BC9B] text-[#19BC9B] bg-[#E6F0F3]">Use Now</a>
+            <div className="divider"></div>
+            <div className="flex gap-2 items-center">
+                <p>Did the code work?</p>
+                <div className="btn btn-sm btn-circle border shadow-md btn-success bg-[#19BC9B] ">
+                    <AiOutlineLike className="text-lg" />
+                </div>
+                <div className="btn btn-circle btn-sm  border shadow-md btn-error">
+                    <AiOutlineDislike className="text-lg" />
+                </div>
+
+            </div>
+
+
         </div>
     );
 };
